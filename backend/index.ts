6cde0940b567fcc57575d3d9e376dba544bb8f86
eomielan <eomielan@gmail.com>
2025-02-26 13:37:30 -0800

    Add connect to mongodb

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,32 @@
 import express, { Request, Response } from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+const client = new MongoClient("mongodb://localhost:27017");
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    client.close();
+  });
 
 const errorHandle = (req: Request, res: Response) => {
   console.error(res.status);
 };
 
 app.use(errorHandle);
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
