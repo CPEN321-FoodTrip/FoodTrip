7063f46310dc1b4f5e080a839e40c282a2df531d
kenny931931 <112777093+kenny931931@users.noreply.github.com>
2025-03-03 11:39:13 -0800

    Basic frontend grocery view (#7)
    1. create basic view of grocery page
    
    2. switch back first view to MainActivity

--- frontend/app/src/main/java/com/example/FoodTripFrontend/GroceryActivity.kt
+++ frontend/app/src/main/java/com/example/FoodTripFrontend/GroceryActivity.kt
@@ -1,12 +1,24 @@
 package com.example.FoodTripFrontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GroceryActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "GroceryActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,31 @@ class GroceryActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backBtn = findViewById<Button>(R.id.back_button);
+        val recipeList = findViewById<LinearLayout>(R.id.recipe_list_layout)
+
+        // Switch back to home page
+        backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val sampleStr = "ingredients"
+
+        for (num in 1..30) {
+            val newTextView = TextView(this)
+            newTextView.textSize = 25f
+            val str = sampleStr + num
+            newTextView.text = str
+
+            newTextView.setOnClickListener {
+                val intent = Intent(applicationContext, PopActivity::class.java)
+                startActivity(intent)
+            }
+
+            recipeList.addView(newTextView);
         }
     }
 }
