3ce559518c6bd460fe19f15c92fe2ad0e3d6d9f3
kenny931931 <kenny931931@gmail.com>
2025-03-03 15:23:38 -0800

    basic view design for account and past trip

--- frontend/app/src/main/java/com/example/FoodTripFrontend/PastTripActivity.kt
+++ frontend/app/src/main/java/com/example/FoodTripFrontend/PastTripActivity.kt
@@ -1,12 +1,22 @@
 package com.example.FoodTripFrontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PastTripActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "GroceryActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,30 @@ class PastTripActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backBtn = findViewById<Button>(R.id.back_button_past)
+        val pastTripList = findViewById<LinearLayout>(R.id.past_trip_list_layout)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val _sampleStr = "trip"
+
+        for (num in 1..30) {
+            val newTextView = TextView(this)
+            newTextView.textSize = 25f
+            val str = _sampleStr + num
+            newTextView.text = str
+
+            newTextView.setOnClickListener {
+                val _text = "show past trip route and details $num"
+                Toast.makeText(this, _text, Toast.LENGTH_SHORT).show()
+            }
+
+            pastTripList.addView(newTextView);
         }
     }
 }
