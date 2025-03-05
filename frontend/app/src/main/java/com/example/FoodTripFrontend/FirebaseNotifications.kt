a52bdbab5f9805884d04a87120dc97ecd4373c4a
eomielan <eomielan@gmail.com>
2025-03-04 21:21:15 -0800

    Add prelim implementation for notifications

--- frontend/app/src/main/java/com/example/FoodTripFrontend/FirebaseNotifications.kt
+++ frontend/app/src/main/java/com/example/FoodTripFrontend/FirebaseNotifications.kt
@@ -1,20 +1,53 @@
 package com.example.FoodTripFrontend
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FirebaseNotifications : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.data.size > 0) {
-            // Handle the data payload
-        }
         if (remoteMessage.notification != null) {
-            // Handle the notification payload
-            remoteMessage.notification!!.body?.let { sendNotification(it) }
+            val title = remoteMessage.notification?.title ?: "New Discount Available!"
+            val body = remoteMessage.notification?.body ?: "Check out the latest discounts!"
+            sendNotification(title, body)
         }
     }
 
-    private fun sendNotification(messageBody: String) {
-        // Use Notification Manager to show a notification to the user
+    private fun sendNotification(title: String, messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        val channelId = "discounts_channel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = NotificationChannel(
+            channelId,
+            "Discounts",
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = "Discount Notifications"
+            enableLights(true)
+            lightColor = Color.RED
+            enableVibration(true)
+        }
+        notificationManager.createNotificationChannel(channel)
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
