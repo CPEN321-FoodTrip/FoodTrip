f88fe17ecc0a0008da966892b48169805a3e8546
eomielan <eomielan@gmail.com>
2025-03-13 18:50:47 -0700

    Add no mock tests for delete

--- backend/__tests__/no_mock/NoMockRoute.test.ts
+++ backend/__tests__/no_mock/NoMockRoute.test.ts
@@ -8,6 +8,43 @@ import { ObjectId } from "mongodb";
 const ROUTES_DB_NAME = "route_data";
 const ROUTES_COLLECTION_NAME = "routes";
 
+const SAMPLE_ROUTE = {
+  start_location: {
+    name: "Vancouver",
+    latitude: 49.2608724,
+    longitude: -123.113952,
+  },
+  end_location: {
+    name: "Toronto",
+    latitude: 43.6534817,
+    longitude: -79.3839347,
+  },
+  stops: [
+    {
+      location: {
+        name: "Regina",
+        latitude: 50.45008,
+        longitude: -104.6178,
+        population: 176183,
+      },
+      distanceFromStart: 1329.071074459746,
+      cumulativeDistance: 1329.071074459746,
+      segmentPercentage: 33.33333333333333,
+    },
+    {
+      location: {
+        name: "Minneapolis",
+        latitude: 44.97997,
+        longitude: -93.26384,
+        population: 410939,
+      },
+      distanceFromStart: 2292.312851021066,
+      cumulativeDistance: 2292.312851021066,
+      segmentPercentage: 66.66666666666666,
+    },
+  ],
+};
+
 describe("Unmocked: POST /routes", () => {
   beforeAll(async () => {
     await initializeGeoNamesDatabase();
@@ -291,48 +328,12 @@ describe("Unmocked: POST /routes", () => {
 });
 
 describe("Unmocked: GET /routes/:id", () => {
-  const SAMPLE_ROUTE = {
-    start_location: {
-      name: "Vancouver",
-      latitude: 49.2608724,
-      longitude: -123.113952,
-    },
-    end_location: {
-      name: "Toronto",
-      latitude: 43.6534817,
-      longitude: -79.3839347,
-    },
-    stops: [
-      {
-        location: {
-          name: "Regina",
-          latitude: 50.45008,
-          longitude: -104.6178,
-          population: 176183,
-        },
-        distanceFromStart: 1329.071074459746,
-        cumulativeDistance: 1329.071074459746,
-        segmentPercentage: 33.33333333333333,
-      },
-      {
-        location: {
-          name: "Minneapolis",
-          latitude: 44.97997,
-          longitude: -93.26384,
-          population: 410939,
-        },
-        distanceFromStart: 2292.312851021066,
-        cumulativeDistance: 2292.312851021066,
-        segmentPercentage: 66.66666666666666,
-      },
-    ],
-  };
-
   // Input:
   // Expected status code:
   // Expected behavior:
   // Expected output:
   test("Retreive valid route", async () => {
+    // setup: inset sample route into db
     const db = client.db(ROUTES_DB_NAME);
     const collection = db.collection(ROUTES_COLLECTION_NAME);
     const result = await collection.insertOne(SAMPLE_ROUTE);
@@ -355,6 +356,15 @@ describe("Unmocked: GET /routes/:id", () => {
   // Expected status code:
   // Expected behavior:
   // Expected output:
+  test("Missing tripID", async () => {
+    const response = await request(app).get("/routes/").expect(400);
+    expect(response.body).toHaveProperty("error", "tripID is required");
+  });
+
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
   test("Invalid tripID format", async () => {
     const tripID = "1234"; // tripID should be a valid ObjectId
     const response = await request(app).get(`/routes/${tripID}`).expect(400);
@@ -365,7 +375,7 @@ describe("Unmocked: GET /routes/:id", () => {
   // Expected status code:
   // Expected behavior:
   // Expected output:
-  test("Route not found", async () => {
+  test("Route not found (Empty db)", async () => {
     const tripID = new ObjectId().toHexString(); // nonexistant tripID
     const response = await request(app).get(`/routes/${tripID}`).expect(404);
     expect(response.body).toHaveProperty("error", "Route not found");
@@ -373,15 +383,105 @@ describe("Unmocked: GET /routes/:id", () => {
 });
 
 describe("Unmocked: DELETE /routes/:id", () => {
-  const tripID = "";
-
   // Input:
   // Expected status code:
   // Expected behavior:
   // Expected output:
   test("Valid deletion", async () => {
-    const response = await request(app).delete(`/routes/${tripID}`).expect(200);
+    // setup: insert sample route
+    const db = client.db(ROUTES_DB_NAME);
+    const collection = db.collection(ROUTES_COLLECTION_NAME);
+    const result = await collection.insertOne(SAMPLE_ROUTE);
+    const tripID = result.insertedId;
+    const originalCount = await collection.countDocuments();
 
+    const response = await request(app).delete(`/routes/${tripID}`).expect(200);
     expect(response.body).toHaveProperty("success", true);
+
+    // check db for deletion
+    const findResult = await collection.findOne({ _id: new ObjectId(tripID) });
+    expect(findResult).toBeNull();
+    const newCount = await collection.countDocuments();
+    expect(newCount).toBe(originalCount - 1);
+  });
+
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Missing tripID", async () => {
+    const db = client.db(ROUTES_DB_NAME);
+    const collection = db.collection(ROUTES_COLLECTION_NAME);
+    const originalCount = await collection.countDocuments();
+
+    const response = await request(app).delete("/routes/").expect(400);
+    expect(response.body).toHaveProperty("error", "tripID is required");
+
+    // verify nothing deleted in db
+    const newCount = await collection.countDocuments();
+    expect(newCount).toBe(originalCount);
+  });
+
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Invalid tripID format", async () => {
+    const db = client.db(ROUTES_DB_NAME);
+    const collection = db.collection(ROUTES_COLLECTION_NAME);
+    const originalCount = await collection.countDocuments();
+
+    const tripID = "1234"; // tripID should be a valid ObjectId
+    const response = await request(app).delete(`/routes/${tripID}`).expect(400);
+    expect(response.body).toHaveProperty("error", "Invalid tripID format");
+
+    // verify nothing deleted in db
+    const newCount = await collection.countDocuments();
+    expect(newCount).toBe(originalCount);
+  });
+
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Delete nonexistant route", async () => {
+    // setup: insert sample route
+    const db = client.db(ROUTES_DB_NAME);
+    const collection = db.collection(ROUTES_COLLECTION_NAME);
+    const result = await collection.insertOne(SAMPLE_ROUTE);
+    const tripID = result.insertedId;
+    const originalCount = await collection.countDocuments();
+
+    const fakeTripID = new ObjectId().toHexString(); // nonexistant tripID
+    const response = await request(app)
+      .delete(`/routes/${fakeTripID}`)
+      .expect(404);
+    expect(response.body).toHaveProperty("error", "Route not found");
+
+    // check db to make sure nothing was deleted
+    const findResult = await collection.findOne({ _id: new ObjectId(tripID) });
+    expect(findResult).not.toBeNull();
+    const newCount = await collection.countDocuments();
+    expect(newCount).toBe(originalCount);
+  });
+
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Delete on empty db", async () => {
+    // no setup, no routes in db
+
+    const tripID = new ObjectId().toHexString(); // nonexistant tripID
+    const response = await request(app).delete(`/routes/${tripID}`).expect(404);
+    expect(response.body).toHaveProperty("error", "Route not found");
+
+    // check db to make sure nothing was deleted
+    const db = client.db(ROUTES_DB_NAME);
+    const collection = db.collection(ROUTES_COLLECTION_NAME);
+    const findResult = await collection.findOne({ _id: new ObjectId(tripID) });
+    expect(findResult).toBeNull();
+    const count = await collection.countDocuments();
+    expect(count).toBe(0);
   });
 });
