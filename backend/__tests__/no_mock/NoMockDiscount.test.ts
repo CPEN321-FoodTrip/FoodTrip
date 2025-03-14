759aa9114c75e4b2b069e394921ea480e874926d
eomielan <eomielan@gmail.com>
2025-03-14 00:47:09 -0700

    Setup template code

---
+++ backend/__tests__/no_mock/NoMockDiscount.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import app from "../../index";
+
+// constants for discounts saved in db
+const DB_NAME = "discounts";
+const COLLECTION_NAME = "discounts";
+
+describe("Unmocked: POST /discounts", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Unmocked: GET /discounts/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Unmocked: GET /discounts", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Unmocked: DELETE /discounts/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
---
+++ backend/__tests__/with_mock/WithMockDiscount.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import app from "../../index";
+
+describe("Mocked: POST /discounts", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Mocked: GET /discounts/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Mocked: GET /discounts", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Mocked: DELETE /discounts/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
