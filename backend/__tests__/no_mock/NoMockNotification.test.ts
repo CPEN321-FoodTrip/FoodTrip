8074c077e13d461dc8ee792678adbb421ac57614
eomielan <eomielan@gmail.com>
2025-03-15 00:10:12 -0700

    Add template code to test files

---
+++ backend/__tests__/no_mock/NoMockNotification.test.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import app from "../../index";
+
+// Interface POST /notification/subscribe
+describe("Unmocked: POST //notification/subscribe", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Valid discount", async () => {});
+});
+
+// Interface POST /notification/unsubscribe
+describe("Unmocked: POST //notification/unsubscribe", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Valid discount", async () => {});
+});
---
+++ backend/__tests__/with_mock/WithMockNotification.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import app from "../../index";
+
+// Interface POST /notification/subscribe
+describe("Mocked: POST //notification/subscribe", () => {
+  // Mocked behavior:
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Valid discount", async () => {});
+});
+
+// Interface POST /notification/unsubscribe
+describe("Mocked: POST //notification/unsubscribe", () => {
+  // Mocked behavior:
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("Valid discount", async () => {});
+});
