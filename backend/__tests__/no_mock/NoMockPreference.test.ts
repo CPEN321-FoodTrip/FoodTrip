3a70dde85002740611c902bb875b3f2f7acc7d66
eomielan <eomielan@gmail.com>
2025-03-15 19:42:51 -0700

    Add test templates

---
+++ backend/__tests__/no_mock/NoMockPreference.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import app from "../../index";
+import { client } from "../../services";
+
+const DB_NAME = "preferences";
+const COLLECTION_NAME = "allergies";
+
+describe("Unmocked: POST /preferences/allergies", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Unmocked: GET /preferences/allergies/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Unmocked: DELETE /preferences/allergies/:id/:allergy", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
---
+++ backend/__tests__/with_mock/WithMockPreference.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+import app from "../../index";
+
+describe("Mocked: POST /preferences/allergies", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Mocked: GET /preferences/allergies/:id", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
+
+describe("Mocked: DELETE /preferences/allergies/:id/:allergy", () => {
+  // Input:
+  // Expected status code:
+  // Expected behavior:
+  // Expected output:
+  test("", async () => {});
+});
