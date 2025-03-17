c0d6d05e9fad2b6dfacef2e2771fd1be6f128f0f
eomielan <eomielan@gmail.com>
2025-03-16 20:09:32 -0700

    Revert

--- backend/helpers/NotificationHelper.ts
+++ backend/helpers/NotificationHelper.ts
@@ -24,7 +24,7 @@ export async function removeTokenFromDb(userID: string): Promise<number> {
     .collection(COLLECTION_NAME)
     .deleteOne({ userID });
 
-  return result.deletedCount ?? 0;
+  return result.deletedCount;
 }
 
 // get user fcm token from database
